3c22050a9086a2ceb2e93c9ecde693c1e89964fb
Thai Tran <thai.tran@lingbook.io>
2025-07-16 22:06:11 +0700

    add dockerfile

--- src/main/java/com/vpbankhackathon/store_and_forward_service/models/dtos/AccountOpening.java
+++ src/main/java/com/vpbankhackathon/store_and_forward_service/models/dtos/AccountOpening.java
@@ -6,7 +6,7 @@ import java.util.UUID;
 
 @Data
 public class AccountOpening {
-    private UUID id = UUID.randomUUID();
+    private Long id = UUID.randomUUID().getMostSignificantBits() & Long.MAX_VALUE; // Generate a positive long ID
     private Long timestamp;
     private String customerName;
     private String customerIdentificationNumber;
