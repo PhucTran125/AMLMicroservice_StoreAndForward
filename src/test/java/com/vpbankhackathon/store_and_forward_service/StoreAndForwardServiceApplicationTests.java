44b5802c81a521167c981348c24e147d9c1d16ff
Tran-Anh-Tuan11 <trantuan1112t@gmail.com>
2025-07-17 21:49:03 +0700

    github action file

--- src/test/java/com/vpbankhackathon/store_and_forward_service/StoreAndForwardServiceApplicationTests.java
+++ src/test/java/com/vpbankhackathon/store_and_forward_service/StoreAndForwardServiceApplicationTests.java
@@ -3,6 +3,7 @@ package com.vpbankhackathon.store_and_forward_service;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+//test github actions
 @SpringBootTest
 class StoreAndForwardServiceApplicationTests {
 
